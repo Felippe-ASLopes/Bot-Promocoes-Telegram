8aba6719371f5cc6b908738f79aea8670dfb20fc
Felippe-ASLopes <felippedias3@gmail.com>
2025-11-27 14:38:29 -0300

    :books: docs: adicionando instruções ao config

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
+# Substitua pelo ID e API_HASH do BOT
 API_ID = 123456
 API_HASH = 'SUA_API_HASH_AQUI'
 USER_IDS = [
-    987654
+    # Substitua pelos IDs dos usuários autorizados a usar o bot
+    123,
+    456
 ]
 
 SESSION_NAME = 'sessao_userbot'
